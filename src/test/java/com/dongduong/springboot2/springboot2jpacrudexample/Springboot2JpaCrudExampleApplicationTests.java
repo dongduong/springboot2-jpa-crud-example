299f074728913a2535e8ce3e795666a161bbdd39
Dong Duong <dong.jobready@gmail.com>
2020-08-23 15:45:29 +0700

    Init project

---
+++ src/test/java/com/dongduong/springboot2/springboot2jpacrudexample/Springboot2JpaCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongduong.springboot2.springboot2jpacrudexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2JpaCrudExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
